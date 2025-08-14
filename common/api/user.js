8ac3b5ab27e5035af042e1e06635fb5f9f550ec4
shiyu <313644503@qq.com>
2025-08-14 21:33:56 +0800

    1

--- common/api/user.js
+++ common/api/user.js
@@ -2,3 +2,6 @@ import http from "@/utils/request.js"
 	
 export const updateUser = (params, config = {}) => http.post('/api/user/update',
 	params, config)
+	
+export const changeConsultant = (params, config = {}) => http.post('/api/user/changeConsultant',
+	params, config)
