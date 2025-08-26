02c35b515bee20cd98444332f36fae4150b1dbd8
shiyu <313644503@qq.com>
2025-08-26 21:58:32 +0800

    1

--- api/consultant.js
+++ api/consultant.js
@@ -3,8 +3,10 @@ import http from "@/utils/request.js"
 export const getConsultantList = (params, config = {}) => http.post('/api/consultant/consultant/getConsultantList',
 	params, config)
 	
-	
 export const getConsultantById = (params, config = {}) => http.post('/api/consultant/consultant/getConsultantById',
+	params, config)
+	
+export const saveConsultant = (params, config = {}) => http.post('/api/consultant/consultant/saveConsultant',
 	params, config)
 	
 	
